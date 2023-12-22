4ad866827ba6364b57074d9e1ccffc9f9664c55b
rdbchry <120248013+rdbchry@users.noreply.github.com>
2023-12-23 06:25:13 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -114,7 +114,7 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
-    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.url = "github:nix-community/home-manager/tree/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-doom-emacs.url = "github:librephoenix/nix-doom-emacs?ref=pgtk-patch";
     stylix.url = "github:danth/stylix";
